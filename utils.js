383e1569f38104f308a80bbd1ff0205e24e08472
Baleksas <alexasbag17@gmail.com>
2023-03-08 11:26:36 +0000

    feat: add utils file where abstracted functions will be used for multiple pages

---
+++ utils.js
@@ -0,0 +1,9 @@
+prepareData = (d) => {
+  let jsonStringified = JSON.stringify(d);
+  jsonStringified = jsonStringified.replace(/{/g, "");
+  jsonStringified = jsonStringified.replace(/}/g, "");
+  jsonStringified = jsonStringified.replace(/"/g, "");
+  jsonStringified = jsonStringified.replace(/,/g, ", ");
+
+  return jsonStringified;
+};
